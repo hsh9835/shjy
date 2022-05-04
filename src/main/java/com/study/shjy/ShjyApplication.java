09e7932cecc0181c7d92e450626a1c84e3110410
hsh9835 <hsh9835@gmail.com>
2022-05-04 21:25:28 +0900

    first commit

---
+++ src/main/java/com/study/shjy/ShjyApplication.java
@@ -0,0 +1,13 @@
+package com.study.shjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShjyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/shjy/ShjyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.shjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShjyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
