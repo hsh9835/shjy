4297de1f4df600a991daa59948cb0b5b90f1e072
hsh9835 <hsh9835@gmail.com>
2022-05-11 21:52:49 +0900

    Second Commit

--- src/main/java/com/study/shjy/ShjyApplication.java
+++ src/main/java/com/study/shjy/ShjyApplication.java
@@ -2,6 +2,7 @@ package com.study.shjy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sehwa.ex2.entitiy.Memo;
 
 @SpringBootApplication
 public class ShjyApplication {
