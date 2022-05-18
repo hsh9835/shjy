5bcbc2eb963c4ba17ed43872c0bc023187c55469
hsh9835 <hsh9835@gmail.com>
2022-05-18 20:20:23 +0900

    Have little miss typed so I changed code.

---
+++ src/main/java/com/study/shjy/Memo.java
@@ -0,0 +1,23 @@
+package sehwa.ex2.entitiy;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_memo")
+@ToString
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+
+} // end class
